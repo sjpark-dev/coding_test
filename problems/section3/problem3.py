0137d8de4159b1c18d7db02a8f0d6ff9951916db
sjpark-dev <sjpark.dev@gmail.com>
2020-10-20 22:13:33 +0900

    fixed sec3 problem3.py and problem4.py

--- problems/section3/problem3.py
+++ problems/section3/problem3.py
@@ -1,21 +1,9 @@
-n = int(input())
-list1 = list(map(int, input().split()))
-m = int(input())
-list2 = list(map(int, input().split()))
-i1 = i2 = 0
-list3 = []
-while i1 < n and i2 < m:
-    if list1[i1] < list2[i2]:
-        list3.append(list1[i1])
-        i1 += 1
-    else:
-        list3.append(list2[i2])
-        i2 += 1
+cards = list(range(1, 21))
 
-if i1 < n:
-    list3 = list3 + list1[i1:]
+for i in range(10):
+    s, e = map(int, input().split())
 
-if i2 < m:
-    list3 = list3 + list2[i2:]
+    for j in range((e-s+1)//2):
+        cards[s-1+j], cards[e-1-j] = cards[e-1-j], cards[s-1+j]
 
-print(*list3)
+print(*cards)
