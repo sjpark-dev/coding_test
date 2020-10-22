3b122b78c9622d16352d411d5938746feb3ba862
sjpark-dev <sjpark.dev@gmail.com>
2020-10-22 18:04:27 +0900

    solved sec5 problem6.py

--- problems/section5/problem6.py
+++ problems/section5/problem6.py
@@ -2,17 +2,17 @@ from collections import deque
 
 if __name__ == '__main__':
     n, m = map(int, input().split())
-    q = deque(zip(map(int, input().split()), range(n)))
+    q = deque([(p, v) for p, v in enumerate(map(int, input().split()))])
     cnt = 0
 
     while True:
-        for i in range(1, len(q)):
-            if q[0][0] < q[i][0]:
-                q.append(q.popleft())
+        cur = q.popleft()
+
+        if any(cur[1] < x[1] for x in q):
+            q.append(cur)
         else:
-            p = q.popleft()
             cnt += 1
 
-            if p[1] == m:
+            if cur[0] == m:
                 print(cnt)
                 break
