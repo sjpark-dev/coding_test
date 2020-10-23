425f06762690381c8c24355729aedfdbb0d02b38
sjpark-dev <sjpark.dev@gmail.com>
2020-10-23 13:55:46 +0900

    solved sec5 problem10.py

---
+++ problems/section5/problem10.py
@@ -0,0 +1,19 @@
+import heapq
+
+if __name__ == '__main__':
+    heap = []
+
+    while True:
+        n = int(input())
+
+        if n == -1:
+            break
+
+        if n == 0:
+            if len(heap) == 0:
+                print(-1)
+            else:
+                print(heapq.heappop(heap))
+            continue
+
+        heapq.heappush(heap, n)
