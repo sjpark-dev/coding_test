78cba75bfd965269b670317c4bd2dfc942ebd26d
sjpark-dev <sjpark.dev@gmail.com>
2020-10-27 14:10:24 +0900

    updated sec6 problem3.py

--- problems/section6/problem3.py
+++ problems/section6/problem3.py
@@ -1,4 +1,4 @@
-def DFS(x):
+def dfs(x):
     if x > n:
         for i in range(1, n+1):
             if check[i] == 1:
@@ -6,12 +6,12 @@ def DFS(x):
         print()
     else:
         check[x] = 1
-        DFS(x+1)
+        dfs(x + 1)
         check[x] = 0
-        DFS(x+1)
+        dfs(x + 1)
 
 
 if __name__ == '__main__':
     n = int(input())
     check = [0] * (n+1)
-    DFS(1)
+    dfs(1)
