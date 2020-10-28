c47e85949ceb7488ca9f10a84cfbb7e6be18fc0a
sjpark-dev <sjpark.dev@gmail.com>
2020-10-28 12:46:11 +0900

    updated sec6 problem4.py

--- problems/section6/problem4.py
+++ problems/section6/problem4.py
@@ -1,32 +1,21 @@
-def dfs(x):
-    global i
-    if x == n:
-        sum = 0
-        for i in range(n):
-            if check[i] == 1:
-                sum += a[i]
-            else:
-                sum -= a[i]
-        if sum == 0:
-            pos[i] = 1
-        i += 1
+import sys
+
+
+def dfs(l, sum):
+    if sum > total // 2:  # 시간 복잡도를 줄이기 위한 코드
         return
+    if l == n:
+        if sum == total-sum:
+            print('YES')
+            sys.exit(0)
     else:
-        check[x] = 1
-        dfs(x+1)
-        check[x] = 0
-        dfs(x+1)
+        dfs(l+1, sum+a[l])
+        dfs(l+1, sum)
 
 
 if __name__ == '__main__':
     n = int(input())
     a = list(map(int, input().split()))
-    check = [0] * n
-    pos = [0] * (2**n)
-    i = 0
-    dfs(0)
-
-    if any(i for i in pos):
-        print('YES')
-    else:
-        print('NO')
+    total = sum(a)
+    dfs(0, 0)
+    print('NO')
