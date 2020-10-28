a585a213cdeac485d2a6362efefd4c73a2ee8066
sjpark-dev <sjpark.dev@gmail.com>
2020-10-28 14:07:28 +0900

    updated sec6 problem5.py

--- problems/section6/problem5.py
+++ problems/section6/problem5.py
@@ -1,24 +1,21 @@
-def dfs(x):
-    global total
-    if x == n:
-        sum = 0
-        for i in range(n):
-            if check[i] == 1:
-                sum += a[i]
-        if total < sum <= c:
-            total = sum
+def dfs(l, sum, tsum):
+    global result
+    if sum + (total-tsum) < result:
         return
+    if sum > c:
+        return
+    if l == n:
+        if sum > result:
+            result = sum
     else:
-        check[x] = 1
-        dfs(x+1)
-        check[x] = 0
-        dfs(x+1)
+        dfs(l+1, sum+a[l], tsum+a[l])
+        dfs(l+1, sum, tsum+a[l])
 
 
 if __name__ == '__main__':
     c, n = map(int, input().split())
     a = [int(input()) for _ in range(n)]
-    check = [0] * n
-    total = 0
-    dfs(0)
-    print(total)
+    total = sum(a)
+    result = 0
+    dfs(0, 0, 0)
+    print(result)
