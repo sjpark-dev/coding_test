e87175e7e3a367eab592407778fe353ead46cdc5
sjpark-dev <sjpark.dev@gmail.com>
2020-11-05 23:52:01 +0900

    solved programmers p9.py

---
+++ problems/programmers/p9.py
@@ -0,0 +1,11 @@
+def solution(A,B):
+    A.sort()
+    B.sort()
+    answer = 0
+    for i in range(len(A)):
+        answer += A[i] * B[len(B)-1-i]
+    return answer
+
+
+if __name__ == '__main__':
+    print(solution([1, 2], [3, 4]))
