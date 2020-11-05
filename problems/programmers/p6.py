2d09d38af19e159614759c4a23eed4b261c72c2c
sjpark-dev <sjpark.dev@gmail.com>
2020-11-05 22:01:01 +0900

    solved programmers p6.py

---
+++ problems/programmers/p6.py
@@ -0,0 +1,20 @@
+def solution(citations):
+    citations.sort()
+    answer = 0
+    for i in range(len(citations), -1, -1):
+        if count(citations, i) >= i:
+            answer = i
+            break
+    return answer
+
+
+def count(a, h):
+    cnt = 0
+    for x in a:
+        if x >= h:
+            cnt += 1
+    return cnt
+
+
+if __name__ == '__main__':
+    print(solution([33, 66]))
